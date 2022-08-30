a20cec9a74c224acfa65331ea7dce2ab4a2993ca
Jefferson Zuanon <jefferson.zuanon@gpnet.com.br>
2022-08-30 10:42:31 -0300

    selecionando autores no select multiplo

--- src/app/livros/formulario-livro/formulario-livro.component.ts
+++ src/app/livros/formulario-livro/formulario-livro.component.ts
@@ -15,21 +15,21 @@ export class FormularioLivroComponent implements OnInit {
 
   id: number | null;
 
-  livroFormGroup: FormGroup;  
-  
+  livroFormGroup: FormGroup;
+
   erroNaRequisicao: string = '';
-  mensagemSemAutorCadastrado: string ='';
+  mensagemSemAutorCadastrado: string = '';
   autores: AutorOutput[] = [];
 
   constructor(
-    private activateRoute: ActivatedRoute, 
-    private formBuilder: FormBuilder, 
+    private activateRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
     private autorService: AutorService,
     private livroService: LivroService,
     private router: Router
-    ) { 
+  ) {
     this.id = this.activateRoute.snapshot.paramMap.get("id") as number | null;
-    
+
     this.livroFormGroup = this.formBuilder.group({
       titulo: ['', Validators.required],
       anoLancamento: ['', Validators.required],
@@ -38,50 +38,55 @@ export class FormularioLivroComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if(this.id){
-      this.livroService.buscaPorId(this.id).subscribe(
-        data=>{
-          this.livroFormGroup = this.formBuilder.group({
-            titulo: [data.titulo, Validators.required],
-            anoLancamento: [data.anoLancamento, Validators.required],
-            autoresIds: [null, Validators.required]
-          });
-        },
-        error=>{
-          if(error?.error?.message){
-            this.erroNaRequisicao = error.error.message;
-          }else{
-            this.erroNaRequisicao = "Ocorreu um erro inesperado. Tem mais tarde, por favor!"
-          }
-        }
-      );
-    }
+
     this.autorService.buscaTodos().subscribe(
       data => {
         if (data.length > 0) {
           this.autores = data;
+          this.buscaLivro();
         } else {
           this.mensagemSemAutorCadastrado = 'Cadastre autores primeiramente!';
         }
       },
       error => {
-        if(error?.error?.message){
+        if (error?.error?.message) {
           this.erroNaRequisicao = error.error.message;
-        }else{
+        } else {
           this.erroNaRequisicao = "Ocorreu um erro inesperado. Tem mais tarde, por favor!"
         }
       }
     );
   }
 
-  cadastrar(){
+  buscaLivro() {
+    if (this.id) {
+      this.livroService.buscaPorId(this.id).subscribe(
+        data => {
+          this.livroFormGroup = this.formBuilder.group({
+            titulo: [data.titulo, Validators.required],
+            anoLancamento: [data.anoLancamento, Validators.required],
+            autoresIds: [data.autores.map(autor=>{return autor.id + ''}), Validators.required]
+          });
+        },
+        error => {
+          if (error?.error?.message) {
+            this.erroNaRequisicao = error.error.message;
+          } else {
+            this.erroNaRequisicao = "Ocorreu um erro inesperado. Tem mais tarde, por favor!"
+          }
+        }
+      );
+    }
+  }
+
+  cadastrar() {
     const livro = this.livroFormGroup.getRawValue() as LivroInput;
     this.livroService.cadastra(livro).subscribe(
-      data=>{
-        const navigationExtras: NavigationExtras =  { state: { successData: `Livro ${data.titulo} alterado com sucesso!` } };
+      data => {
+        const navigationExtras: NavigationExtras = { state: { successData: `Livro ${data.titulo} alterado com sucesso!` } };
         this.router.navigate(["livros", navigationExtras])
       },
-      error=>{
+      error => {
         if (error?.error?.message) {
           this.erroNaRequisicao = error.error.message;
         } else {
@@ -92,17 +97,17 @@ export class FormularioLivroComponent implements OnInit {
 
   }
 
-  alterar(){
-    if(!this.id){
+  alterar() {
+    if (!this.id) {
       return;
     }
     const livro = this.livroFormGroup.getRawValue() as LivroInput;
     this.livroService.altera(this.id, livro).subscribe(
-      data=>{
-        const navigationExtras: NavigationExtras =  { state: { successData: `Livro ${data.titulo} cadastrado com sucesso!` } };
+      data => {
+        const navigationExtras: NavigationExtras = { state: { successData: `Livro ${data.titulo} cadastrado com sucesso!` } };
         this.router.navigate(["livros", navigationExtras])
       },
-      error=>{
+      error => {
         if (error?.error?.message) {
           this.erroNaRequisicao = error.error.message;
         } else {
